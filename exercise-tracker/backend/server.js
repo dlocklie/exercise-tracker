df95b3cd62302703df7d27eb5dca9bc149350cd8
Drew Lockliear <drewlockliear@drews-air.lan>
2023-09-07 16:36:03 -0500

    Port back to 5001

--- exercise-tracker/backend/server.js
+++ exercise-tracker/backend/server.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
