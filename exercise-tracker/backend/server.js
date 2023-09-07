a8cc0622ab5c882fdd3faf1f430f0351b3b0b249
Drew Lockliear <drewlockliear@drews-air.lan>
2023-09-07 14:12:30 -0500

    working now

---
+++ exercise-tracker/backend/server.js
@@ -0,0 +1,35 @@
+var apm = require('elastic-apm-node').start({
+    serviceName: 'exercise-tracker-api',
+    secretToken: 'xU2Zey2QBRekZflwNQ',
+    serverUrl: 'https://c4de3462d44a446f9cd959c9a9c4a631.apm.southcentralus.azure.elastic-cloud.com:443',
+    environment: 'my-environment'
+});
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+const exerciseRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
---
+++ exercise-tracker/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercises from "./components/edit-exercise.component";
+// import CreateExercise from "./componets/create-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Routes>
+          <Route path="/" element={<ExercisesList />} />
+          <Route path="/edit/:id" element={<EditExercises />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
